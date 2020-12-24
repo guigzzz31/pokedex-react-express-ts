4ff811652a50c404a41e21ca684b4e8fb09cf747
guigzzz31 <guillaume.trouche.6@gmail.com>
2020-12-24 11:24:52 +0100

    server fix

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {Db as MongoDb, MongoClient} from 'mongodb'
 const cors = require('cors')
 const path = require('path')
 import mongoose from "mongoose";
@@ -6,18 +7,27 @@ import routes from "./routes/router";
 const app = express();
 
 //Database
-mongoose
-  .connect("mongodb://127.0.0.1:27017/pokedexV1", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    autoIndex: true,
-  })
-  .then(() => console.log("Connected to database"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/pokedexV1", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     autoIndex: true,
+//   })
+//   .then(() => console.log("Connected to database"))
+//   .catch((err) => console.log(err));
 
+
+// const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://guigz:<kaioken>@cluster0.rhs7n.mongodb.net/<pokedexV1>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("pokedexv1").collection("pokemons");
+  // perform actions on the collection object
+  client.close();
+});
 //middleware
-app.use(cors())
+//app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
