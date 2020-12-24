61429e3ecb2311e012981f7a606f95e065158c4d
guigzzz31 <guillaume.trouche.6@gmail.com>
2020-12-24 14:49:02 +0100

    server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+import bodyParser from "body-parser";
 import express from "express";
-import {Db as MongoDb, MongoClient} from 'mongodb'
+import {Db as MongoDb} from 'mongodb'
 const cors = require('cors')
 const path = require('path')
 import mongoose from "mongoose";
 import routes from "./routes/router";
 const app = express();
+
+// app.use(bodyParser.json());
+app.use(cors())
 
 //Database
 // mongoose
@@ -17,21 +21,16 @@ const app = express();
 //   .then(() => console.log("Connected to database"))
 //   .catch((err) => console.log(err));
 
-
-// const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://guigz:<kaioken>@cluster0.rhs7n.mongodb.net/<pokedexV1>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("pokedexv1").collection("pokemons");
-  // perform actions on the collection object
-  client.close();
-});
+const connection = "mongodb+srv://guigz:kaioken@cluster0.rhs7n.mongodb.net/pokedexV1?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err))
 //middleware
-//app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')))
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
@@ -40,5 +39,6 @@ app.get('*', (req, res) => {
 app.use(routes);
 
 // start server
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server started on ${port}`));
