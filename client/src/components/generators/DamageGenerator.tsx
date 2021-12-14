4babe21aa949392e4303f2630d857a318a280588
guigzzz31 <guillaume.trouche.6@gmail.com>
2021-12-14 17:40:57 +0100

    fixes on damageGen

--- client/src/components/generators/DamageGenerator.tsx
+++ client/src/components/generators/DamageGenerator.tsx
@@ -1,41 +1,9 @@
 import React from "react";
 
 import { Box, Grid, Typography } from "@mui/material";
-import { createStyles, makeStyles, withStyles } from '@mui/styles';
-import { createTheme, Theme } from "@mui/material/styles";
-
 import CustomToUpperCase from "../../hooks/CustomToUpperCase";
 import ParseColorDamage from "../../hooks/ParseColorDamage";
 import ParseColorDamageValue from "../helpers/ParseColorDamageValue";
-
-const styles = ({ palette, spacing }: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      //padding: spacing.unit,
-      backgroundColor: palette.background.default,
-      color: palette.primary.main,
-    },
-    typeBox: {
-      borderRadius: 9,
-      border: 1,
-      borderWidth: 2,
-      borderColor: "#000",
-      width: 80,
-    },
-    typeText: {
-      color: "#FFF",
-      fontWeight: 900,
-      fontSize: 16,
-    },
-    typeValue: {
-      color: "#FFF",
-      fontWeight: 800,
-      fontSize: 16,
-    }
-  });
-//withStyles(styles);
 
 const DamageGenerator = (damages: any) => {
   const keys = Object.keys(damages.damages);
