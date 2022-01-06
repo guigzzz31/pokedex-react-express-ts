f32f214da4ed86a85126230effc40618658837df
guigzzz31 <guillaume.trouche.6@gmail.com>
2022-01-06 09:51:54 +0100

    damages fix direction responsive

--- client/src/components/generators/DamageGenerator.tsx
+++ client/src/components/generators/DamageGenerator.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material";
 import CustomToUpperCase from "../../hooks/CustomToUpperCase";
 import ParseColorDamage from "../../hooks/ParseColorDamage";
 import ParseColorDamageValue from "../helpers/ParseColorDamageValue";
@@ -8,73 +8,76 @@ import ParseColorDamageValue from "../helpers/ParseColorDamageValue";
 const DamageGenerator = (damages: any) => {
   const keys = Object.keys(damages.damages);
   const values = Object.values(damages.damages);
+  const matches = useMediaQuery('(min-width:1300px)');
+
   return (
-    <Box sx={{ display: "flex" }}>
-      <Grid direction="column" spacing={1} container>
+    <Box sx={{ display: "flex", width: "100%", flexDirection: !matches ? "row" : "column", justifyContent: "center" }}>
+      <Box sx={{ display: "flex", flexDirection: !matches ? "column" : "row" }}>
         {keys
           ? keys.map((key: string, i) => {
             return (
-              <Grid key={i} item>
-                <Box
+              <Box
+                key={i}
+                sx={{
+                  borderRadius: 8,
+                  border: 1,
+                  borderWidth: 2,
+                  borderColor: "transparent",
+                  width: 80,
+                  bgcolor: ParseColorDamage(key),
+                  margin: 0.5
+
+                }}
+              >
+                <Typography
+                  key={key}
                   sx={{
-                    borderRadius: 8,
-                    border: 1,
-                    borderWidth: 2,
-                    borderColor: "transparent",
-                    width: 80,
-                    bgcolor: ParseColorDamage(key)
+                    color: "#FFF",
+                    fontWeight: 900,
+                    fontSize: 16,
                   }}
+                  align="center"
                 >
-                  <Typography
-                    key={key}
-                    sx={{
-                      color: "#FFF",
-                      fontWeight: 900,
-                      fontSize: 16,
-                    }}
-                    align="center"
-                  >
-                    {CustomToUpperCase(key)}
-                  </Typography>
-                </Box>
-              </Grid>
+                  {CustomToUpperCase(key)}
+                </Typography>
+              </Box>
             );
           })
           : null}
-      </Grid>
-      <Grid direction="column" spacing={1} container>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: !matches ? "column" : "row" }}>
         {values
           ? values.map((value: any, i) => {
             return (
-              <Grid key={i} item>
-                <Box
+              <Box
+                key={i}
+                sx={{
+                  borderRadius: 8,
+                  border: 1,
+                  borderWidth: 2,
+                  backgroundColor: "#FFF",
+                  borderColor: ParseColorDamageValue(value),
+                  width: 80,
+                  margin: 0.5
+                }}
+              >
+                <Typography
+                  key={value}
+                  align="center"
+                  style={{ color: ParseColorDamageValue(value) }}
                   sx={{
-                    borderRadius: 8,
-                    border: 1,
-                    borderWidth: 2,
-                    backgroundColor: "#FFF",
-                    borderColor: ParseColorDamageValue(value),
-                    width: 80,
+                    color: "#FFF",
+                    fontWeight: 800,
+                    fontSize: 16,
                   }}
                 >
-                  <Typography
-                    key={value}
-                    align="center"
-                    style={{ color: ParseColorDamageValue(value) }}
-                    sx={{
-                      color: "#FFF",
-                      fontWeight: 800,
-                      fontSize: 16,
-                    }}
-                  >
-                    {CustomToUpperCase(value)}
-                  </Typography>
-                </Box>
-              </Grid>
+                  {CustomToUpperCase(value)}
+                </Typography>
+              </Box>
             );
           })
           : null}
-      </Grid>
+      </Box>
     </Box>
   );
 }
